71d8f26c5dd8f8878a8d43a090bc8256f0dedd36
mirajkc <kcmiraj11@gmail.com>
2026-01-15 15:47:50 +0545

    created the global error handle and not found handler

---
+++ client/app/error.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <main className="h-screen w-full flex flex-col justify-center items-center py-16 md:py-0">
+      <div className="flex flex-col items-center justify-center text-sm max-md:px-4">
+        <h1 className="text-8xl md:text-9xl font-bold text-indigo-500">500</h1>
+        <div className="h-1 w-16 rounded bg-indigo-500 my-5 md:my-7"></div>
+        <p className="text-2xl md:text-3xl font-bold text-gray-800">Something went wrong</p>
+        <p className="text-sm md:text-base mt-4 text-gray-500 max-w-md text-center">Some unknown error occurred.</p>
+        <div className="flex items-center gap-4 mt-6">
+          <Link href="/" className="bg-gray-800 hover:bg-black px-7 py-2.5 text-white rounded-md active:scale-95 transition-all">
+            Return Home
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
