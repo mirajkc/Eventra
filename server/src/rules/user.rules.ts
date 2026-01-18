00d4adc86c4d6d9d03f36f40da2f023b5a300b5d
mirajkc <kcmiraj11@gmail.com>
2026-01-18 14:01:58 +0545

    reverted the server updated as the bug was from client and not from the server

--- server/src/rules/user.rules.ts
+++ server/src/rules/user.rules.ts
@@ -3,4 +3,4 @@ import Joi from "joi"
 export const userUpdateDTO = Joi.object({
   name: Joi.string().min(5).max(25).allow(null),
   phone: Joi.string().min(10).max(14).allow(null),
-}).unknown(true)
+})
