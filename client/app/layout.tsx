99517ab2396ed072cd2410ac102fc15bc7bbe7ae
Miraj KC <157137590+mirajkc@users.noreply.github.com>
2026-01-29 21:26:26 +0545

    Changed the default theme from system to light

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <ReduxProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
