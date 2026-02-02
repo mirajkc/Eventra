7ad1786479dbed0c72478287a81ae17d4199c360
mirajkc <kcmiraj11@gmail.com>
2026-02-02 12:55:12 +0545

    updated cors policy to allow the api request from admin dashbaord and also development for admin-dashboard frontend started

--- server/src/config/express.config.ts
+++ server/src/config/express.config.ts
@@ -12,7 +12,7 @@ import enviroment from "./enviroment.config.js";
 const app: Application = express()
 
 app.use(cors({
-  origin: [enviroment.clientURL ,"http://localhost:3000" ],
+  origin: [enviroment.clientURL, "http://localhost:3000", "http://localhost:5173"],
   credentials: true
 }));
 app.use(helmet())
